0218c87b34b18403c84b04b5afb85366a472c02c
deepjoshi11th <deepjoshi11th@gmail.com>
2025-06-29 10:09:38 +0530

    Added vite config js

---
+++ profile-v/vite.config.js
@@ -0,0 +1,19 @@
+// vite.config.js
+import { defineConfig } from 'vite';
+import { resolve } from 'path';
+
+export default defineConfig({
+  server: {
+    port: 3000,       // optional: change port if needed
+    open: true        // auto-opens in default browser
+  },
+  build: {
+    outDir: 'dist',    // output directory for production build
+    rollupOptions: {
+        input: {
+            main: resolve(__dirname, 'index.html'), // main entry point
+            era2025: resolve(__dirname, 'src/era/2025/index.html') // specific era entry point
+        },
+    }
+  }
+});
