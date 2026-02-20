b6ca6af4868b29a4e7eef7c536b607f7e46d1188
deepjoshi11th <deepjoshi11th@gmail.com>
2026-02-20 17:30:06 +0530

    feat(eras): update 1990 and 1995 era pages
    Adds historically accurate 1990 page. Adds JS features to 1995 page with Dharmik/Deep birthday joke. Enables time slider.

--- src/components/TimeSlider.js
+++ src/components/TimeSlider.js
@@ -1,19 +1,17 @@
 export function createTimeSlider() {
-    const slider = document.createElement("input");
-    slider.type = "range";
-    slider.min = 1990;
-    slider.max = 2025;
-    slider.step = 5;
-    slider.value = 2025;
-    slider.disabled = true;
-  
-    slider.oninput = () => {
-      const event = new CustomEvent("era-change", {
-        detail: { year: slider.value }
-      });
-      window.dispatchEvent(event);
-    };
-  
-    return slider;
-  }
-  
+  const slider = document.createElement("input");
+  slider.type = "range";
+  slider.min = 1990;
+  slider.max = 2025;
+  slider.step = 5;
+  slider.value = 2025;
+
+  slider.oninput = () => {
+    const event = new CustomEvent("era-change", {
+      detail: { year: slider.value }
+    });
+    window.dispatchEvent(event);
+  };
+
+  return slider;
+}
